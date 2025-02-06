d06ee99af2cdc62e274c9a8c4ba9454dcb79c5d4
omgshokh <shohruz199905@gmail.com>
2025-02-06 16:00:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let r1 = prompt('String typedagi malumotni ,kiriting ')
+console.log(r1)
+
+
+let r2 = prompt('number typedagi malumotkarni kiriting')
+console.log(r2);
+
+
+let r3 = prompt('Boolen typedagi malumotkarni kiriting')
+console.log(r3);
+
+
+
+let r4 = confirm('haqmma malumotlarni togri kiritganingizga ishonasizmi')
+console.log(r4);
+
+
+
+alert(`Siz kiritgan ${r1} so'zi srting typega tegishli
+       Siz kiritgan ${r2} so'zi number typega tegishli
+       siz kiritgan ${r3} so'zi boolen typega tegishli`)
+  
+    
+
